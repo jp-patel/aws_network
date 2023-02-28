2852b3f69b05e1183881bb433d7d0c90bb6f91b6
EC2 Default User <ec2-user@ip-172-31-60-231.ec2.internal>
2023-02-28 06:10:30 +0000

    Adding my module

---
+++ globalvars/output.tf
@@ -0,0 +1,12 @@
+# Default tags
+output "default_tags" {
+  value = {
+    "Owner" = "jalpatel"
+    "App"   = "Web"
+  }
+}
+
+# Prefix to identify resources
+output "prefix" {
+  value     = "week5"
+}
